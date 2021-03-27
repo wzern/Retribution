28283a89cbd01b620edffc2a4194f437943339a7
William Zernikow <william.zernikow@gmail.com>
2021-03-27 23:56:30 +1300

    Zombie Spawn 2 Axis

--- Assets/Scripts/ZombieSpawnerScript.cs
+++ Assets/Scripts/ZombieSpawnerScript.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 
 public class ZombieSpawnerScript : MonoBehaviour
 {
-    public GameObject Zombie;
-    float randx;
-    Vector2 whereToSpawn;
-    public float spawnRate = 2f;
-    float nextSpawn = 0.0f;
+    //initialise values
+    public GameObject Zombie; //set gameobject for zombie
+    public float spawnRate = 2f; //delay in seconds (f) between zombie spawns
+    public int ZombieCountMax; //max amount of zombies at a time
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float randx; //for random x position
+    float randy; //for random y position
+    float nextSpawn = 0.0f; //time until new zombie is spawned
+    int AmountOfZombies = 0; //amount of zombies spawned
 
+    Vector2 whereToSpawn; //vector2 to determine spawn position
+   
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > nextSpawn)
+        //if zombie spawn cooldown over and amount of zombies is less than max, then do the following
+        if (Time.time > nextSpawn && AmountOfZombies < ZombieCountMax)
         {
-            nextSpawn = Time.time + spawnRate;
-            randx = Random.Range(-8.4f, 8.4f);
-            whereToSpawn = new Vector2(randx, transform.position.y);
-            Instantiate(Zombie, whereToSpawn, Quaternion.identity);
+            nextSpawn = Time.time + spawnRate; //restart zombie spawn cooldown
+            randx = Random.Range(-8.4f, 8.4f); //generate random x position
+            randy = Random.Range(-8f, 8f); //generate random y position
+            whereToSpawn = new Vector2(randx, randy); //tell zombie where to spawn
+            Instantiate(Zombie, whereToSpawn, Quaternion.identity); //spawn the zombie at given location
+            AmountOfZombies += 1; //increase zombie count by one each time one is spawned
         }
     }
 }
