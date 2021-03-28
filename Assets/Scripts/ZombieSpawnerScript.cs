86144769da6d36b3977b7b37e41c8364e9922554
William Zernikow <william.zernikow@gmail.com>
2021-03-28 13:59:44 +1300

    Update ZombieSpawnerScript.cs

--- Assets/Scripts/ZombieSpawnerScript.cs
+++ Assets/Scripts/ZombieSpawnerScript.cs
@@ -8,15 +8,9 @@ public class ZombieSpawnerScript : MonoBehaviour
     public GameObject Zombie; //set gameobject for zombie
     public float spawnRate = 2f; //delay in seconds (f) between zombie spawns
     public int ZombieCountMax; //max amount of zombies at a time
-
-    float randx; //for random x position
-    float randy; //for random y position
     float nextSpawn = 0.0f; //time until new zombie is spawned
     int AmountOfZombies = 0; //amount of zombies spawned
-    int randSquare;
-
-    Vector2 whereToSpawn; //vector2 to determine spawn position
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -24,40 +18,47 @@ public class ZombieSpawnerScript : MonoBehaviour
         if (Time.time > nextSpawn && AmountOfZombies < ZombieCountMax)
         {
             nextSpawn = Time.time + spawnRate; //restart zombie spawn cooldown
-
-            randSquare = Random.Range(1, 7);
-            switch (randSquare)
-            {
-                case 1:
-                    randx = Random.Range(-10.5f, -4.7f); //generate random x position
-                    randy = Random.Range(11.5f, 1.7f); //generate random y position
-                    break;
-                case 2:
-                    randx = Random.Range(-3.4f, 13.2f); //generate random x position
-                    randy = Random.Range(9.4f, 7.7f); //generate random y position
-                    break;
-                case 3:
-                    randx = Random.Range(1.8f, 3f); //generate random x position
-                    randy = Random.Range(6.3f, 5f); //generate random y position
-                    break;
-                case 4:
-                    randx = Random.Range(-3.15f, 5f); //generate random x position
-                    randy = Random.Range(3.3f, -4f); //generate random y position
-                    break;
-                case 5:
-                    randx = Random.Range(8.8f, 10.3f); //generate random x position
-                    randy = Random.Range(3.3f, -2.1f); //generate random y position
-                    break;
-                case 6:
-                    randx = Random.Range(11.9f, 17.2f); //generate random x position
-                    randy = Random.Range(5.3f, -2.15f); //generate random y position
-                    break;
-            }
-
-            whereToSpawn = new Vector2(randx, randy); //tell zombie where to spawn
-            Instantiate(Zombie, whereToSpawn, Quaternion.identity); //spawn the zombie at given location
+            Instantiate(Zombie, RandomPos(), Quaternion.identity); //spawn the zombie at given location
             AmountOfZombies += 1; //increase zombie count by one each time one is spawned
         }
+    }
+
+    static Vector2 RandomPos()
+    {
+        float randX = 0; //for random x position
+        float randY = 0; //for random y position
+        Vector2 whereToSpawn; //vector2 to determine spawn position
+
+        int randSquare = Random.Range(1, 7); //choose which square to spawn zombie in
+        switch (randSquare)
+        {
+            case 1:
+                randX = Random.Range(-10.5f, -4.7f); //generate random x position for square 1
+                randY = Random.Range(11.5f, 1.7f); //generate random y position for square 1
+                break;
+            case 2:
+                randX = Random.Range(-3.4f, 13.2f); //generate random x position for square 2
+                randY = Random.Range(9.4f, 7.7f); //generate random y position for square 2
+                break;
+            case 3:
+                randX = Random.Range(1.8f, 3f); //generate random x position for square 3
+                randY = Random.Range(6.3f, 5f); //generate random y position for square 3
+                break;
+            case 4:
+                randX = Random.Range(-3.15f, 5f); //generate random x position for square 4
+                randY = Random.Range(3.3f, -4f); //generate random y position for square 4
+                break;
+            case 5:
+                randX = Random.Range(8.8f, 10.3f); //generate random x position for square 5
+                randY = Random.Range(3.3f, -2.1f); //generate random y position for square 5
+                break;
+            case 6:
+                randX = Random.Range(11.9f, 17.2f); //generate random x position for square 6
+                randY = Random.Range(5.3f, -2.15f); //generate random y position for square 6
+                break;
+        }
+        whereToSpawn = new Vector2(randX, randY); //tell zombie where to spawn
+        return whereToSpawn;
     }
 }
 
@@ -69,9 +70,3 @@ public class ZombieSpawnerScript : MonoBehaviour
 // sq4 x: -3.15 y: 3.27 to x: 5 y: -4 \/
 // sq5 x: 8.81 y: 3.31 to x: 10.28 y: -2.11 \/
 // sq6 x: 11.89 y: 5.28 to x: 17.17 y: -2.15 \/
-//
-//
-//
-//
-//
-//
